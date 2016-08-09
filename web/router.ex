9d2d2ec18dd4d26e0508ee57d70347ddb5abef16
Zhuo-Fei Hui <zhfhui+github@gmail.com>
2016-08-09 19:55:48 +0200

    Fix router and user controller

--- web/router.ex
+++ web/router.ex
@@ -17,13 +17,8 @@ defmodule Rumbl.Router do
   scope "/", Rumbl do
     pipe_through :browser # Use the default browser stack
 
-    get "/",               PageController, :index
-    resources "/users",    UserController, only: [:index, :show, :new, :create]
+    get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", Rumbl do
-  #   pipe_through :api
-  # end
 end
