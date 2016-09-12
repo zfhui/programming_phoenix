58f6a64e17b101553156e10bbd65e677614219f1
Zhuo-Fei Hui <zhfhui+github@gmail.com>
2016-09-12 18:28:46 +0200

    Add player js and video css

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -13,6 +13,16 @@
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
 
+import Player from "./player"
+let video = document.getElementById("video")
+
+if(video) {
+  Player.init(video.id, video.getAttribute("data-player-id"),
+  () => {
+    console.log("player ready!")
+  })
+}
+
 // Import local files
 //
 // Local files can be imported directly using relative
